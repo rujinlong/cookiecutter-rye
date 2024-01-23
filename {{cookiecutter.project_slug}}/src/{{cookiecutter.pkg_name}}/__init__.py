4fb0fbbb447ff674e7b733ae1bf10f83d097a995
rujinlong <jinlongru@gmail.com>
2024-01-23 20:06:37 +0100

    add typer

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.pkg_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.pkg_name}}/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for {{ cookiecutter.project_name }}."""
 
+from .cli import app
+
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
--- {{cookiecutter.project_slug}}/src/{{cookiecutter.pkg_name}}/cli.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.pkg_name}}/cli.py
@@ -1,8 +1,8 @@
 """Console script for {{cookiecutter.pkg_name}}."""
 
-{% if cookiecutter.command_line_interface|lower == 'click' -%}
-import click
+{%- if cookiecutter.command_line_interface|lower == 'click' %}
 
+import click
 
 @click.command()
 def main():
@@ -14,4 +14,22 @@ def main():
 
 if __name__ == "__main__":
     main()  # pragma: no cover
+{%- endif %}
+
+
+{%- if cookiecutter.command_line_interface|lower == "typer" %}
+
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def main():
+    """Main entrypoint."""
+    typer.echo("{{ cookiecutter.project_slug }}")
+    typer.echo("=" * len("{{ cookiecutter.project_slug }}"))
+    typer.echo("{{ cookiecutter.project_short_description }}")
+
+if __name__ == "__main__":
+    app()  # pragma: no cover
 {%- endif %}
